1f17a95c950a8e6dcf6f8ed919540484e40265f9
Vu Ky <vukynpro@gmail.com>
2025-12-07 23:43:40 +0700

    feat: add migration for app_services table with index

--- db/history/sqlite/sqlite.go
+++ db/history/sqlite/sqlite.go
@@ -89,4 +89,33 @@ var Migrations = []models.Migration{
 			return err
 		},
 	},
+	{
+		Name: "004_create_app_services_table",
+		Up: func(db *bun.DB) error {
+			_, err := db.Exec(`
+				CREATE TABLE IF NOT EXISTS app_services (
+					id TEXT PRIMARY KEY NOT NULL,
+					app_code TEXT UNIQUE NOT NULL,
+					app_name TEXT NOT NULL,
+					app_secret TEXT NOT NULL,
+					callback_url TEXT NOT NULL,
+					status INTEGER DEFAULT 1 NOT NULL
+				)
+			`)
+			if err != nil {
+				return err
+			}
+
+			// Create index
+			_, err = db.Exec(`CREATE INDEX IF NOT EXISTS app_services_app_code_idx ON app_services (app_code)`)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+		Down: func(db *bun.DB) error {
+			_, err := db.Exec(`DROP TABLE IF EXISTS app_services`)
+			return err
+		},
+	},
 }
